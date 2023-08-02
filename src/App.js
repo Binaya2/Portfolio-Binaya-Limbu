1abe9d03fce753f5e4843e7f6ea0d4c94e9c80d6
Binaya2 <137303838+Binaya2@users.noreply.github.com>
2023-08-02 15:40:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css'
+import React, { useState, useEffect } from 'react';
+// import DeleteButton from './Components/DeleteButton';
+// import { timeStamp, countWords } from './functions';
+import NoteCard from './Components/NoteCard/NoteCard';
+import NoteForm from './Components/NoteForm';
+
+function App() {
+
+
+  const [notes, setNotes] = useState(() => {
+    const localList = localStorage.getItem("notes")
+
+    if (localList === null) {
+      return []
+    } else {
+      return JSON.parse(localList)
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
+
+
+
+
+  function setCurrentNote() {
+    // if(note.content.trim() !== ""){
+    //   window.alert("This action will delete the current note!")
+    //   return
+    // }
+
+    // setNote(notes.find( e => e.id === id ));
+    // setCount(notes.find( e => e.id === id ).wordCount)
+    // setNotes(notes.filter(e => e.id !== id))
+
+  }
+
+
+  return (
+    <>
+      <div className='app'>
+        <NoteForm notes={notes} setNotes={setNotes} />
+
+        <div className='container'>
+          {notes.map((n, i) => <NoteCard note={n} key={i} notes={notes} setNotes={setNotes} setCurrentNote={setCurrentNote} />)}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
